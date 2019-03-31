0e789c25b5f51a89c321339e17f7a9e3b22d1f39
mainpc <jmzahn@satx.rr.com>
2019-03-31 18:33:34 -0500

    split data

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import numpy as np
 
 lr = LogReg
 Data = lr.getData(lr, 'mushrooms.csv')
-predictions = lr.logReg(lr, Data, 0.01)
+permutation = np.random.permutation(Data.shape[1])
+trainData, testData = Data[permutation][6500:], Data[permutation][:6500]
+predictions = lr.logReg(lr, trainData, 0.01)
