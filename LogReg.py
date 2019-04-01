1321760d52832f2a8476316a033df34ff10e8ff5
mainpc <jmzahn@satx.rr.com>
2019-03-31 19:04:18 -0500

    datatype changes, transpostition fixes

--- LogReg.py
+++ LogReg.py
@@ -16,21 +16,21 @@ class LogReg:
         # Gets shape of data and subtracts labels row
         mushShape = np.array(self.rawMushData.shape)
         mushShape[1] = mushShape[1] - np.intc(1)
-        self.mushData = np.zeros(mushShape, dtype = np.intc)
+        self.mushData = np.zeros(mushShape, dtype = np.double)
 
         # Converts discreet letter values to ints
         for self.itt in range (mushShape[0]):
-            mushDataTemp = np.zeros(mushShape[1], dtype = np.intc)
+            mushDataTemp = np.zeros(mushShape[1], dtype = np.double)
             dimVals = np.zeros(26, dtype = np.unicode_)
-            valCounter = np.intc(0)
+            valCounter = np.double(0)
             for self.itt2 in range (mushShape[1]):
                 for self.itt3 in range (26):
                     if self.rawMushData[self.itt, self.itt2 + np.intc(1)] == dimVals[self.itt3]:
-                        mushDataTemp[self.itt2] = self.itt3
+                        mushDataTemp[self.itt2] = np.double(self.itt3)
                         break
                     elif dimVals[self.itt3] == np.zeros(1, dtype = np.unicode_):
-                        mushDataTemp[self.itt2] = valCounter
-                        valCounter = valCounter + np.intc(1)
+                        mushDataTemp[self.itt2] = np.double(valCounter)
+                        valCounter = valCounter + np.double(1)
                         dimVals[self.itt3] = self.rawMushData[self.itt, self.itt2 + np.intc(1)]
                         break
             self.mushData[self.itt] = mushDataTemp
@@ -39,13 +39,13 @@ class LogReg:
     #this function is doing the regression, should only take dataset and hyperParameters
     def logReg(self, dataset, learningRate):
         shapeOData = np.array(dataset.shape)
-        self.Ws = np.zeros((shapeOData[0]-1,1), dtype=np.double)
+        self.Ws = np.zeros((shapeOData[1]-1,1), dtype=np.double)
         bias = np.zeros((1,1), dtype=np.double)
-        self.Xs = dataset[1:].T
+        self.Xs = dataset[:][1:]
         #self.Ts = np.zeros((shapeOData[1],1), dtype=np.intc)
-        self.Ts = np.array(dataset[0]).T
-        self.Ts = self.Ts.reshape(shapeOData[1], 1)
-        self.yPred = np.ones((shapeOData[1],1),dtype=np.intc)
+        self.Ts = np.array(dataset[:][0])
+        self.Ts = self.Ts.reshape(shapeOData[0], 1)
+        self.yPred = np.ones((shapeOData[0],1),dtype=np.intc)
         m = len(self.Ts)
         #for self.itt in range(np.shape(self.Xs)[0]):
             #self.Xs[self.itt][0] = 1
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 lr = LogReg
 Data = lr.getData(lr, 'mushrooms.csv')
-permutation = np.random.permutation(Data.shape[1])
+Data = Data.T
+permutation = np.random.permutation(Data.shape[0])
 trainData, testData = Data[permutation][6500:], Data[permutation][:6500]
 predictions = lr.logReg(lr, trainData, 0.01)
